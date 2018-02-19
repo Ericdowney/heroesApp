da2c7d15b3425ab384db3cb8d16254c27735535d
edowney <edowney@icct.com>
2018-02-19 10:34:20 -0500

    Removed urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,9 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-// heroesUrl: 'https://uhfxxujepg.execute-api.us-east-1.amazonaws.com/dev/heroes',
 export const environment = {
     production: false,
-    heroesUrl: 'https://uhfxxujepg.execute-api.us-east-1.amazonaws.com/dev/heroes',
-    heroGroupsUrl: 'https://3c52fab9ag.execute-api.us-east-1.amazonaws.com/dev/herogroups',
-    heroSkillsUrl: 'https://4vpran6rbd.execute-api.us-east-1.amazonaws.com/dev/heroskills',
+    heroesUrl: '',
+    heroGroupsUrl: '',
+    heroSkillsUrl: '',
 };
